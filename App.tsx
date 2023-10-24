a56e6d66f30e69575dc94c9c555031b9ea0cfefd
caesarlinsa <caesar_linsa@163.com>
2023-10-24 21:22:12 +0800

    将导航独立

--- App.tsx
+++ App.tsx
@@ -11,60 +11,14 @@ import {
     View,
     Text
 } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native'
-import { createBottomTabNavigator} from "@react-navigation/bottom-tabs";
-import Icon from 'react-native-vector-icons/FontAwesome'
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
-import Detail  from "./components/Detail";
-import List  from "./components/List";
+import Navigation from "./components/Navigation";
 
-export type BottomTabList = {
-  Home: undefined,
-  Detail: undefined
-}
 
-const Tab = createBottomTabNavigator<BottomTabList>()
 function App(): JSX.Element {
-  return (
-        <NavigationContainer>
-          <Tab.Navigator>
-            <Tab.Screen name={'Home'} component={List} options={{tabBarLabel: '首页',
-              tabBarIcon: ({color, size}) => (
-              <Icon name={'home'} size={size} color={color} />
-              )
-            }}></Tab.Screen>
-            <Tab.Screen name={'Detail'} component={Detail} options={{tabBarLabel: '详情页',
-              tabBarIcon: ({color, size}) => (
-                  <MaterialIcons name={'details'} size={size} color={color} />
-              )}}></Tab.Screen>
-          </Tab.Navigator>
-        </NavigationContainer>
-
-  );
+    return (
+          <Navigation/>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'red',
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignItems: 'flex-end'
-
-  },
-  v1: {
-    borderColor: 'dark',
-    borderWidth: 1,
-    width: 50,
-    height: 50
-  },
-  v2: {
-    borderColor: 'dark',
-    borderWidth: 1,
-    width: 50,
-    height: 50,
-    alignSelf: 'flex-start'
-  }
-});
 
 export default App;
---
+++ components/Navigation.tsx
@@ -0,0 +1,36 @@
+import React, {Component} from "react";
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+// @ts-ignore
+import Icon from "react-native-vector-icons/FontAwesome";
+// @ts-ignore
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import Detail  from "./Detail";
+import List  from "./List";
+
+export type BottomTabList = {
+    Home: undefined,
+    Detail: undefined
+}
+const Tab = createBottomTabNavigator<BottomTabList>()
+export default class Navigation extends Component {
+    render() {
+        return (
+            <NavigationContainer>
+                <Tab.Navigator>
+                    <Tab.Screen name={'Home'} component={List} options={{tabBarLabel: '首页',
+                        tabBarIcon: ({color, size}) => (
+                            <Icon name={'home'} size={size} color={color} />
+                        )
+                    }}></Tab.Screen>
+                    <Tab.Screen name={'Detail'} component={Detail} options={{tabBarLabel: '详情页',
+                        tabBarIcon: ({color, size}) => (
+                            <MaterialIcons name={'details'} size={size} color={color} />
+                        )}}></Tab.Screen>
+                </Tab.Navigator>
+            </NavigationContainer>
+
+        )
+    }
+
+}
